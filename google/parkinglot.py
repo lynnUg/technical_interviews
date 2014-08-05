7dd21cece93e8b61c7634f65dbec85612aca280c
lynnug <lynnasiimwe@gmail.com>
2014-08-05 13:08:22 +0300

    adding parking lot problem

---
+++ google/parkinglot.py
@@ -0,0 +1,40 @@
+def findPark2(cars,index):
+	
+	left=cars[:index+1]
+	right=cars[index:]
+	print left, right
+	swap=0
+	Found=False
+	i=len(left)-1
+	print i
+	while (i>0) & (len(left)>1):
+		print "test"
+		if left[i-1]!='-':
+			temp=left[i]
+			left[i]=left[i-1]
+			left[i-1]=temp
+			swap+=1
+		else:
+			Found=True
+			swap+=1
+			return swap
+		print left
+		i-=1
+	if not Found:
+		swap=0
+	j=0
+	print "here"
+	while (j<len(right)-1) & (len(right)>1) & (not Found):
+		if right[j+1]!='-':
+			temp=right[j]
+			right[j]=right[j+1]
+			right[j+1]=temp
+			swap+=1
+		else:
+			swap+=1
+			return swap
+		j+=1
+		print right
+		print j
+	return "no empty park"
+print findPark2([1,2,3,4,5,"-",7],0)
